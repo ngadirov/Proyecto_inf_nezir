458166feff07fdde152e2e7e6ae6b47a9568193f
ngadirov <nezirgadirov@gmail.com>
2022-11-24 21:20:25 +0100

    Colliders (interacción con el mundo)

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+        //qualitysettings.vsynccount = 0;
+        //application.targetframerate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
+
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 100.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 100.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
 }
